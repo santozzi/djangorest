ebae675dece38472a5bed363290b32ebfbb47cef
Sergio Antozzi <santozzi@gmail.com>
2023-07-23 01:19:42 -0300

    migrates

--- projects/models.py
+++ projects/models.py
@@ -5,5 +5,5 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     technology = models.CharField(max_length=200)
-    creted_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     
