1234cfea004577841541e0d031b7dfad63cb1ef4
Sergio Antozzi <santozzi@gmail.com>
2023-07-23 00:53:56 -0300

    modifico alowed host

--- universo3d/settings.py
+++ universo3d/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',default='secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = 'RENDER' not in os.environ
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://djangorest-ymkh.onrender.com/']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
 if RENDER_EXTERNAL_HOSTNAME:
