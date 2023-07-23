f2f3368f37ed24d1ff42fce5e493e15cf5cf1718
Sergio Antozzi <santozzi@gmail.com>
2023-07-23 00:36:17 -0300

    corrijo STATICFILES_STORAGE

--- universo3d/settings.py
+++ universo3d/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFileStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     
 # Default primary key field type
